81475b2db502fd7843f9541f595511ef9e7e63dd
Marvin Hülsmann <59929827+marvinhuelsmann@users.noreply.github.com>
2021-02-13 01:35:57 +0100

    init commit

---
+++ marvins-link-shorter/background.js
@@ -0,0 +1,18 @@
+chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+    function (tabs) {
+        var tab = tabs[0].url
+        if (tab !== "chrome://newtab/"
+            && tab !== "chrome://extensions/"
+            && tab !== "undefined"
+            && tab !== ""
+            && tab !== null
+            && tab !== "chrome://extensions/?id=dlcndoeajhdcfloijgjgblhbdbpcladh"
+            && tab !== "about:blank"
+            && tab !== "https://chrome.google.com/webstore/detail/marvins-link-shorter/apibgginpfhgioddcidkijcckipplmoo/related?hl=de&authuser=0"
+            && tav !== "https://chrome.google.com/webstore/detail/marvins-link-shorter/apibgginpfhgioddcidkijcckipplmoo/related?hl=de") {
+
+            window.open("https://directly.click/create?url=" + tab, "_blank")
+
+        }
+    }
+);
